pixeldustproject-caf/android_frameworks_opt_slimrecent

Adapt for our Utils.
Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/systemui/slimrecent/AppIconLoader.java
@@ -34,7 +34,7 @@ import java.lang.ref.WeakReference;
 
 import com.android.systemui.slimrecent.icons.IconsHandler;
 
-import com.aicp.gear.util.ImageHelper;
+import com.android.internal.util.pixeldust.slimrecents.ImageHelper;
 
 /**
  * This class handles async app icon load for the requested apps

src/com/android/systemui/slimrecent/AppSidebar.java
@@ -48,10 +48,10 @@ import com.android.systemui.R;
 
 import java.util.ArrayList;
 
-import com.aicp.gear.util.Action;
-import com.aicp.gear.util.ActionConfig;
-import com.aicp.gear.util.ActionConstants;
-import com.aicp.gear.util.ActionHelper;
+import com.android.internal.util.pixeldust.slimrecents.Action;
+import com.android.internal.util.pixeldust.slimrecents.ActionConfig;
+import com.android.internal.util.pixeldust.slimrecents.ActionConstants;
+import com.android.internal.util.pixeldust.slimrecents.ActionHelper;
 
 public class AppSidebar extends FrameLayout {
     private static final String TAG = "SlimRecentAppSidebar";

src/com/android/systemui/slimrecent/RecentController.java
@@ -104,7 +104,7 @@ import com.android.systemui.statusbar.phone.StatusBar;
 
 import static com.android.systemui.statusbar.phone.StatusBar.SYSTEM_DIALOG_REASON_RECENT_APPS;
 
-import com.aicp.gear.util.ImageHelper;
+import com.android.internal.util.pixeldust.slimrecents.ImageHelper;
 
 /**
  * Our main recents controller.

src/com/android/systemui/slimrecent/icons/IconsHandler.java
@@ -48,7 +48,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import com.aicp.gear.util.AbstractIconsHandler;
+import com.android.internal.util.pixeldust.slimrecents.AbstractIconsHandler;
 
 public class IconsHandler implements AbstractIconsHandler {
 

aoleary/frameworks_opt_slimrecent

src/com/android/systemui/slimrecent/AppIconLoader.java
@@ -34,7 +34,7 @@ import java.lang.ref.WeakReference;
 
 import com.android.systemui.slimrecent.icons.IconsHandler;
 
-import com.aicp.gear.util.ImageHelper;
+import com.android.internal.util.bootleggers.slimrecents.ImageHelper;
 
 /**
  * This class handles async app icon load for the requested apps

src/com/android/systemui/slimrecent/AppSidebar.java
@@ -48,10 +48,10 @@ import com.android.systemui.R;
 
 import java.util.ArrayList;
 
-import com.aicp.gear.util.Action;
-import com.aicp.gear.util.ActionConfig;
-import com.aicp.gear.util.ActionConstants;
-import com.aicp.gear.util.ActionHelper;
+import com.android.internal.util.bootleggers.slimrecents.Action;
+import com.android.internal.util.bootleggers.slimrecents.ActionConfig;
+import com.android.internal.util.bootleggers.slimrecents.ActionConstants;
+import com.android.internal.util.bootleggers.slimrecents.ActionHelper;
 
 public class AppSidebar extends FrameLayout {
     private static final String TAG = "SlimRecentAppSidebar";

src/com/android/systemui/slimrecent/RecentController.java
@@ -104,7 +104,7 @@ import com.android.systemui.statusbar.phone.StatusBar;
 
 import static com.android.systemui.statusbar.phone.StatusBar.SYSTEM_DIALOG_REASON_RECENT_APPS;
 
-import com.aicp.gear.util.ImageHelper;
+import com.android.internal.util.bootleggers.slimrecents.ImageHelper;
 
 /**
  * Our main recents controller.

src/com/android/systemui/slimrecent/icons/IconsHandler.java
@@ -48,7 +48,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import com.aicp.gear.util.AbstractIconsHandler;
+import com.android.internal.util.bootleggers.slimrecents.AbstractIconsHandler;
 
 public class IconsHandler implements AbstractIconsHandler {
 